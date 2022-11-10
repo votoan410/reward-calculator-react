0c1fa54dc4b8adae45bbed6bd31923f1f98ef12c
Toan <votoan410@gmail.com>
2022-11-10 14:51:41 -0600

    adjust code based on first code review suggestion

--- src/api/fetchTransactions.js
+++ src/api/fetchTransactions.js
@@ -1,9 +1,5 @@
 export default function fetchTransactions() {
   const endpoint = "http://localhost:3000/transactions";
 
-  return fetch(endpoint)
-    .then((response) => response.json())
-    .then((jsonData) => {
-      return jsonData;
-    });
+  return fetch(endpoint).then((response) => response.json());
 }
--- src/components/RewardTable.js
+++ src/components/RewardTable.js
@@ -1,85 +1,82 @@
 import { useState, useEffect } from "react";
 import calculateRewardPoints from "../utils/pointsCalculate";
 
-export default function RewardTable(props) {
+export default function RewardTable({
+  transactionData = null,
+  loading = false,
+}) {
   const [totalPoints, setTotalPoints] = useState({});
-
-  const { transactionData, loading } = props;
 
   useEffect(() => {
     let userTotalPay = {};
-    if (transactionData) {
-      // map user to their respective total pay
-      transactionData.forEach((transaction) => {
-        // add user and current points to the new object
-        if (!userTotalPay.hasOwnProperty(transaction.userId)) {
-          userTotalPay[transaction.userId] = calculateRewardPoints(
-            transaction.value
-          );
-        } 
-        // if the user already exists within the new object, then just accumulate the point
-        else {
-          userTotalPay[transaction.userId] += calculateRewardPoints(
-            transaction.value
-          );
-        }
-      });
-    }
+    // map user to their respective total pay
+    transactionData?.forEach((transaction) => {
+      // add user and current points to the new object
+      if (!userTotalPay.hasOwnProperty(transaction.userId)) {
+        userTotalPay[transaction.userId] = calculateRewardPoints(
+          transaction.transactionPrice
+        );
+      }
+      // if the user already exists within the new object, then just accumulate the point
+      else {
+        userTotalPay[transaction.userId] += calculateRewardPoints(
+          transaction.transactionPrice
+        );
+      }
+    });
+
     setTotalPoints(userTotalPay);
   }, [transactionData]);
 
-  return (
+  return loading ? (
+    <div>Table is waiting for data...</div>
+  ) : (
     <>
-      {loading ? (
-        <div>Table is waiting for data...</div>
-      ) : (
-        <>
-          <table data-testid="table">
-            <thead>
-              <tr>
-                <th>user Id</th>
-                <th> Transaction Date</th>
-                <th className="table__header-green"> Payment Value $$$</th>
-                <th className="table__header-yellow"> Calculated Rewards</th>
-              </tr>
-            </thead>
-            <tbody>
-              {transactionData.map((transaction, idx) => (
-                // use index as key in this case because that is the only stable and unique data
-                // for the current application
-                <tr key={idx}>
-                  <td>{transaction.userId}</td>
-                  <td>
-                    {new Date(transaction.date).toDateString()}{" "}
-                    {new Date(transaction.date).toTimeString()}
-                  </td>
-                  <td>${transaction.value}</td>
-                  <td>{calculateRewardPoints(transaction.value)}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <table data-testid="table">
-            <thead>
-              <tr>
-                <th>User Id</th>
-                <th className="table__header-yellow">
-                  Total Calculated Reward Points
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* the USER ID will be used as key in here since it will be unique in this case */}
-              {Object.keys(totalPoints).map((entry) => (
-                <tr key={entry}>
-                  <td>{entry}</td>
-                  <td>{totalPoints[entry]}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </>
-      )}
+      <table data-testid="table">
+        <thead>
+          <tr>
+            <th>user Id</th>
+            <th> Transaction Date</th>
+            <th className="table__header-green"> Payment Value $$$</th>
+            <th className="table__header-yellow"> Calculated Rewards</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactionData.map((transaction, idx) => (
+            // use index as key in this case because that is the only stable and unique data
+            // for the current application
+            // update: previously, I used index which is NOT GOOD, updated it with transactionId
+            <tr key={transaction.transactionId}>
+              <td>{transaction.userId}</td>
+              <td>
+                {new Date(transaction.date).toDateString()}{" "}
+                {new Date(transaction.date).toTimeString()}
+              </td>
+              <td>${transaction.transactionPrice}</td>
+              <td>{calculateRewardPoints(transaction.transactionPrice)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <table data-testid="table">
+        <thead>
+          <tr>
+            <th>User Id</th>
+            <th className="table__header-yellow">
+              Total Calculated Reward Points Per User Id
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* the USER ID will be used as key in here since it will be unique in this case */}
+          {Object.keys(totalPoints).map((userId) => (
+            <tr key={userId}>
+              <td>{userId}</td>
+              <td>{totalPoints[userId]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
